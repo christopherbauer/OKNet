3366b3f5d3016f218a52c1723e5e85973365c4ad
Christopher Bauer <Christopher.Bauer@innRoad.com>
2019-02-14 11:29:57 -0500

    Add execution state to stop sleep

--- OKNet/src/OKNet.App/App.xaml.cs
+++ OKNet/src/OKNet.App/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Windows;
 using OKNet.App.ViewModel;
 using OKNet.Core;
@@ -12,8 +13,21 @@ namespace OKNet.App
     /// </summary>
     public partial class App : Application
     {
+        [Flags]
+        public enum EXECUTION_STATE : uint
+        {
+            ES_AWAYMODE_REQUIRED = 0x00000040,
+            ES_CONTINUOUS = 0x80000000,
+            ES_DISPLAY_REQUIRED = 0x00000002,
+            ES_SYSTEM_REQUIRED = 0x00000001
+            // Legacy flag, should not be used.
+            // ES_USER_PRESENT = 0x00000004
+        }
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
         public App()
         {
+            SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS | EXECUTION_STATE.ES_AWAYMODE_REQUIRED);
         }
     }
 }
