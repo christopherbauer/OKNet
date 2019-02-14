c50ddf99a4131e8a630b7400c6230597080377ad
Christopher Bauer <Christopher.Bauer@innRoad.com>
2019-02-14 13:41:25 -0500

    Add debug property and displays assembly number for tracking which version is deployed

---
+++ OKNet/src/OKNet.App/Converter/BoolToVisibilityConverter.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace OKNet.App.Converter
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null && (bool) value ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null && (Visibility)value == Visibility.Visible ? true : false;
+        }
+    }
+}
