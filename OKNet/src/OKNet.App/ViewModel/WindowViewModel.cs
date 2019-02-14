5d28be6c99f7ad0c1a307b3ef485e6e1b0575e53
Christopher Bauer <Christopher.Bauer@innRoad.com>
2019-02-14 13:41:25 -0500

    Add debug property and displays assembly number for tracking which version is deployed

--- OKNet/src/OKNet.App/ViewModel/WindowViewModel.cs
+++ OKNet/src/OKNet.App/ViewModel/WindowViewModel.cs
@@ -1,15 +1,27 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Reflection;
+using System.Windows;
 
 namespace OKNet.App.ViewModel
 {
     public class WindowViewModel : ViewModelBase
     {
         private ObservableCollection<ViewModelBase> _windows;
+        private bool _isDebugMode;
 
         public ObservableCollection<ViewModelBase> Windows
         {
             get => _windows;
             set => SetValue(ref _windows, value);
+        }
+
+        public string AssemblyVersion => $"v{Assembly.GetExecutingAssembly().GetName().Version}";
+
+        public bool IsDebugMode
+        {
+            get => _isDebugMode;
+            set => SetValue(ref _isDebugMode, value);
         }
     }
 }
